611e05c72049e13dfe12468b3922ef521a4eb0af
yocccc <stellaxastrum@icloud.com>
2024-11-16 23:40:00 +1100

    stage2 プロトタイプ

--- client.py
+++ client.py
@@ -1,63 +1,108 @@
 import socket
+import threading
 
 def main():
-    host_room = {}
-    join_room = {}
     try:
         server_address="localhost"
         server_port=9000
-        #TCPでリクエスト送信
-        tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        tcp_socket.connect((server_address,server_port))
-        #1でルーム作成
-        print("Which operation do you want to do")
-        print("1: Create a chat room")
-        print("2: Join a chat room")
-        operation=input(">")
-        while operation !="1" and operation !="2":
-            print("Invalid operation. Enter 1 or 2")
-            operation = input(">")
-        #room name 入力
-        room_name = input("Enter room name: ")
-        room_name_size = len(room_name.encode())
-        #room name が256バイト以下か確認
-        if room_name_size > 2**8:
-            print("Room name must not be over 256 bytes")
-            print("FYI: This room name has "+ room_name_size + "bytes")
-            room_name = input("Re-enter room name: ")
+
+        while True:
+            #TCPでリクエスト送信
+            tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            tcp_socket.connect((server_address,server_port))
+            local_ip, local_port = tcp_socket.getsockname()  # ローカルIPとポート番号を取得
+            #1でルーム作成print
+            ("Which operation do you want to do")
+            print("1: Create a chat room")
+            print("2: Join a chat room")
+            print("3: exit")
+            operation=input(">")
+            if operation == "3":
+                break
+            while operation !="1" and operation !="2":
+                print("Invalid operation. Enter 1 or 2")
+                operation = input(">")
+            #room name 入力
+            room_name = input("Enter room name: ")
             room_name_size = len(room_name.encode())
-        #user name 入力
-        user_name = input("Enter user name: ")
-        state="0"
-        operation_payload=user_name
-        operation_payload_size=len(operation_payload.encode())
-        header = room_name_size.to_bytes(1,"big") + operation.encode() + state.encode() + operation_payload_size.to_bytes(29,"big")
-        tcp_socket.sendall(header)
-        tcp_socket.sendall(room_name.encode()+operation_payload.encode())
+            #room name が256バイト以下か確認
+            if room_name_size > 2**8:
+                print("Room name must not be over 256 bytes")
+                print("FYI: This room name has "+ room_name_size + "bytes")
+                room_name = input("Re-enter room name: ")
+                room_name_size = len(room_name.encode())
+            #user name 入力
+            user_name = input("Enter user name: ")
+            state="0"
+            operation_payload=user_name
+            operation_payload_size=len(operation_payload.encode())
+            header = room_name_size.to_bytes(1,"big") + operation.encode() + state.encode() + operation_payload_size.to_bytes(29,"big")
+            tcp_socket.sendall(header)
+            tcp_socket.sendall(room_name.encode()+operation_payload.encode())
 
-        if operation == "1":
-            header = tcp_socket.recv(30)
-            status = header[:1].decode()
-            operation_payload_size = int.from_bytes(header[1:],"big")
-            operation_payload = tcp_socket.recv(operation_payload_size)
-            host_token = operation_payload.decode()
-            host_room[room_name] = host_token
+            if operation == "1":
+                header = tcp_socket.recv(30)
+                status = header[:1].decode()
+                operation_payload_size = int.from_bytes(header[1:],"big")
+                operation_payload = tcp_socket.recv(operation_payload_size).decode()
+                if status =="1":
+                    token = operation_payload
+                    print("successfully created")
+                elif status =="2":
+                    print(operation_payload)
 
-        elif operation == "2":
-            status=tcp_socket.recv(5).decode()
-            if status =="error":
-                print("this room does not exist.")
-            else:
-                operation_payload_size_bytes = tcp_socket.recv(29)
-                operation_payload_size=int.from_bytes(operation_payload_size_bytes,"big")
-                operation_payload = tcp_socket.recv(1)
-                token = operation_payload.decode()
-                join_room[room_name] = token
-                tcp_socket.close()
+            elif operation == "2":
+                header = tcp_socket.recv(30)
+                status = header[:1].decode()
+                operation_payload_size=int.from_bytes(header[1:],"big")
+                operation_payload = tcp_socket.recv(operation_payload_size).decode()
+                if status =="2":
+                    print(operation_payload)
+                    continue
+                else:
+                    token = operation_payload
+            tcp_socket.close()
+            send_message(room_name,token,local_port)
 
+    #2でルーム参加
+    except ChildProcessError:
+        pass
 
-        #2でルーム参加
-    except Exception as e:
-        print(e)
-        print("Connection Failed.")
+def send_message(room_name,token,local_port):
+    packet_size = 4096
+
+    # UDPソケット
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(("",local_port))
+    server_address = "localhost"
+    server_port = 9001
+
+    room_name_size = len(room_name.encode())
+    token_size = len(token.encode())
+
+    available_message_size=packet_size - (token_size + room_name_size)
+
+    threading.Thread(target=receive,daemon=True, args=(udp_socket,)).start()
+
+    while True:
+        # Enter message
+        message = input("Enter message you want to send: ")
+        message_size=len(message.encode())
+        while message_size>available_message_size:
+            message = input("Too long. Re-Enter message you want to send: ")
+            message_size=len(message.encode())
+        udp_socket.sendto(room_name_size.to_bytes(1, "big")+token_size.to_bytes(1, "big")+(room_name+token+message).encode(),(server_address,server_port))
+
+def receive(udp_socket):
+    packet_size = 4096
+    while True:
+        packet, _ = udp_socket.recvfrom(packet_size)
+        user_name_size = int.from_bytes(packet[:1],"big")
+        user_name = packet[1:user_name_size+1].decode()
+        message = packet[user_name_size+1:].decode()
+        print("\n"+user_name+": "+message)
+
+if __name__ == "__main__":
+    main()
+
 main()
--- server.py
+++ server.py
@@ -1,97 +1,146 @@
 import socket
 import threading
-
-chat_room={}
-user_host_token = {}
-user_token = {}
-user_addresses ={}
-host_token= 0
-
-def main():
-
-    token=0
-    server_address=""
-    tcp_port=9000
-    udp_port=9001
-
-    tcp_thread = threading.Thread(target=handle_room,args=(server_address,tcp_port))
-    udp_thread = threading.Thread(target=handler_chat,args=(server_address,udp_port))
-    tcp_thread.start()
+import time
 
 
-#TCP接続でルーム作成、接続を扱う
-def handle_room(server_address,tcp_port):
-    tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    tcp_socket.bind((server_address,tcp_port))
-    tcp_socket.listen()
-    while True:
-        connection,address = tcp_socket.accept()
-        #header
-        header = connection.recv(32)
-        room_name_size = int.from_bytes(header[:1],"big")
-        operation = header[1:2].decode()
-        state = header[2:3].decode()
-        operation_payload_size = int.from_bytes(header[3:],"big")
-        print(room_name_size)
-        print(operation)
-        print(state)
-        print(operation_payload_size)
 
-        body = connection.recv(room_name_size+operation_payload_size)
-        room_name = body[:room_name_size].decode()
-        operation_payload = body[room_name_size:].decode()
+class Server:
+    def __init__(self):
+        self.chat_room={} #{ room名:[ {参加者のtoken : [user name,user_address]} ] }
+        self.user_host_token = {} #host token : user name
+        self.host_token= 0
+        self.address=""
+        self.tcp_port =9000
+        self.udp_port=9001
+        self.token = 1000
+        self.user_last_chat_times={}
+        self.buffer_size = 4096
+        self.timeout_interval = 600 #秒数
 
-        print(room_name)
-        print(operation_payload)
+    def run(self):
+        threading.Thread(target = self.handle_room).start()
+        threading.Thread(target = self.handle_chat).start()
 
-        if operation == "1":
-            state = "1"
-            user_name = operation_payload
-            if room_name not in chat_room:
-                chat_room[room_name] = []
-            chat_room[room_name].append(str(host_token))
-            user_host_token[str(host_token)]= user_name
-            user_addresses[str(host_token)] = address
-            operation_payload = str(host_token)
-            operation_payload_size = len(operation_payload.encode())
-            header = state.encode() + operation_payload_size.to_bytes(29,"big")
-            connection.sendall(header)
-            connection.sendall(operation_payload.encode())
-            print(chat_room)
-            print(user_host_token)
-            host_token+=1
+    #TCP接続でルーム作成、接続を扱う
+    def handle_room(self):
+        tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        tcp_socket.bind((self.address,self.tcp_port))
+        tcp_socket.listen()
+        while True:
+            connection,user_address = tcp_socket.accept()
+            #header
+            header = connection.recv(32)
+            room_name_size = int.from_bytes(header[:1],"big")
+            operation = header[1:2].decode()
+            state = header[2:3].decode()
+            operation_payload_size = int.from_bytes(header[3:],"big")
 
-        elif operation=="2":
-            user_name = operation_payload
-            print(chat_room)
-            if room_name not in chat_room:
-                state ="error"
-                connection.sendall(state.encode())
-            else:
-                operation_payload = str(token)
+            body = connection.recv(room_name_size+operation_payload_size)
+            room_name = body[:room_name_size].decode()
+            operation_payload = body[room_name_size:].decode()
+            print("operation", operation)
+            print("room name",room_name)
+            print("payload",operation_payload)
+
+            if operation == "1":
+                user_name = operation_payload
+                if room_name  in self.chat_room:
+                    state =  "2"
+                    operation_payload = "This room name is already used. Use another one."
+                    operation_payload_size=len(operation_payload.encode())
+                else:
+                    state = "1"
+                    self.chat_room[room_name] = []
+                    self.chat_room[room_name].append({str(self.host_token):[user_name,user_address]})
+                    self.user_host_token[str(self.host_token)]= [user_name,user_address]
+                    operation_payload = str(self.host_token)
+                    self.host_token+=1
                 operation_payload_size = len(operation_payload.encode())
-                connection.sendall(operation_payload_size.to_bytes(29,"big"))
+                header = state.encode() + operation_payload_size.to_bytes(29,"big")
+                connection.sendall(header)
                 connection.sendall(operation_payload.encode())
-                token+=1
+                print(self.chat_room)
 
-# UDPでメッセージの受信、マルチキャスト送信を行う
-def handler_chat(server_address,udp_port):
-    buffer_size = 4096
-    room_name_size_buffer = 1
-    token_size_buffer = 1
-    header_size = room_name_size_buffer + token_size_buffer
-    udp_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    udp_socket.bind((server_address,udp_port))
-    while True:
-        data,address = udp_socket.recv(buffer_size)
-        header = data[:header_size]
-        room_name_size = header[:room_name_size_buffer]
-        token_size = header[room_name_size_buffer:room_name_size_buffer+token_size_buffer]
-        body = data[header_size:]
-        room_name = body[:room_name_size]
-        token_name = body[room_name_size:room_name_size+token_size]
-        message = body[room_name_size+token_size:]
+            elif operation=="2":
+                user_name = operation_payload
+                print("追加前",self.chat_room)
+                if room_name not in self.chat_room:
+                    state ="2"
+                    operation_payload = "This room does not exist."
+                else:
+                    state = "1"
+                    self.chat_room[room_name].append({str(self.token):[user_name,user_address]})
+                    operation_payload = str(self.token)
+                    self.token+=1
+                operation_payload_size = len(operation_payload.encode())
+                header = state.encode() + operation_payload_size.to_bytes(29,"big")
+                connection.sendall(header)
+                connection.sendall(operation_payload.encode())
+                print("追加後",self.chat_room)
+
+
+    # UDPでメッセージの受信、マルチキャスト送信を行う
+    def handle_chat(self):
+        # UDPソケット
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_socket.bind((self.address, self.udp_port))
+
+        room_name_size_buffer = 1
+        token_size_buffer = 1
+        header_size = room_name_size_buffer + token_size_buffer
+
+        print('\nwaiting to receive message')
+
+        threading.Thread(target=self.check_time, daemon=True).start()
+
+        while True:
+            # メッセージの受信
+            data,address = udp_socket.recvfrom(self.buffer_size)
+            header = data[:header_size]
+            room_name_size = int.from_bytes(header[:room_name_size_buffer],"big")
+            token_size = int.from_bytes(header[room_name_size_buffer:room_name_size_buffer+token_size_buffer],"big")
+            body = data[header_size:]
+            room_name = body[:room_name_size].decode()
+            token = body[room_name_size:room_name_size+token_size].decode()
+            message = body[room_name_size+token_size:].decode()
+
+            if room_name not in self.chat_room:
+                state = "2"
+                udp_socket.sendto(state.encode(),address)
+            else:
+                self.chat_room[room_name]
+                token_found = any(token in user for user in self.chat_room[room_name])
+                if token_found:
+                    state="1"
+                    print("message",message)
+                    receivers = []
+                    for participant in self.chat_room[room_name]:
+                        exclude_token, user_info = next(iter(participant.items()))
+                        if exclude_token== token:
+                            sender_name = user_info[0]
+                        else:
+                            receivers.append(user_info[1])
+                    for receiver in receivers:
+                        #このままじゃpacket_size超える可能性ある
+                        user_name_size = len(sender_name.encode())
+                        udp_socket.sendto(user_name_size.to_bytes(1,"big")+(sender_name+message).encode(),receiver)
+                else:
+                    state = "2"
+                    mes ="You don't have proper token probably because of timeout. First participate in the room."
+                    udp_socket.sendto(state.encode(),address)
+
+    def check_time(self):
+        while True:
+            users_to_delete = []
+            current_time = time.time()
+            for user, last_send_time in self.user_last_chat_times.items():
+                if current_time - last_send_time >= self.timeout_interval:
+                    users_to_delete.append(user)
+            for user in users_to_delete:
+                del self.user_last_chat_times[user]
+            time.sleep(self.timeout_interval)
 
 
 if __name__=="__main__":
-    main()
+    server = Server()
+    server.run()
